bb781424fd33089aedc8d2dc50be791b0ac49781
Esther Min <estherjmin@gmail.com>
2020-12-10 13:41:44 -0500

    added addItemKeyUp function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let addBtn = document.querySelector("#addBtn");
 let groceryInput = document.querySelector("#grocery-input");
 let groceryList = [];
 
-// Function definition to add a new item to the grocery list
+// Function definition to add a new item to the grocery list when add button is clicked
 let addItem = (event) => {
     event.preventDefault();
 
@@ -17,5 +17,19 @@ let addItem = (event) => {
     console.log(groceryList);
 }
 
+// Function definition to add a new item to the grocery list when enter key is pressed
+let addItemKeyUp = (event) => {
+    // If the grocery item input is not an empty string, the item will be added to the groceryList array and then the input field is cleared
+    if (groceryInput.value !== "" && event.keyCode === 13) {
+        groceryList.push(groceryInput.value.trim());
+    
+        groceryInput.value = "";
+    }
+   
+    console.log(groceryList);
+}
+
 // When the "+" button is clicked, the addItem function will kick off
 addBtn.addEventListener("click", addItem);
+
+groceryInput.addEventListener("keyup", addItemKeyUp);
