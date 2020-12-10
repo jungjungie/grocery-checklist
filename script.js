e92301745a40782f8a2ab151166c4dd9a59345c0
Esther Min <estherjmin@gmail.com>
2020-12-10 14:06:22 -0500

    added function to save items to localStorage

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let addBtn = document.querySelector("#addBtn");
 let groceryInput = document.querySelector("#grocery-input");
 let groceryList = [];
 
+// Function to save new item to localStorage
+let saveItem = () => {
+    localStorage.setItem("groceries", JSON.stringify(groceryList));
+}
+
 // Function definition to add a new item to the grocery list when add button is clicked
-let addItem = (event) => {
+let addItem = event => {
     event.preventDefault();
 
     // If the grocery item input is not an empty string, the item will be added to the groceryList array and then the input field is cleared
@@ -13,20 +18,24 @@ let addItem = (event) => {
     
         groceryInput.value = "";
     }
-   
-    console.log(groceryList);
+    // console.log(groceryList);
+    
+    // Save to localStorage
+    saveItem();
 }
 
 // Function definition to add a new item to the grocery list when enter key is pressed
-let addItemKeyUp = (event) => {
+let addItemKeyUp = event => {
     // If the grocery item input is not an empty string, the item will be added to the groceryList array and then the input field is cleared
     if (groceryInput.value !== "" && event.keyCode === 13) {
         groceryList.push(groceryInput.value.trim());
     
         groceryInput.value = "";
     }
-   
-    console.log(groceryList);
+    // console.log(groceryList);
+
+    // Save to localStorage
+    saveItem();
 }
 
 // When the "+" button is clicked, the addItem function will kick off
